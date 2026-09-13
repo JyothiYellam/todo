f82ece08ba0943c5f46a8fec8745fcdd0116abd7
JyothiYellam <jyothiym23@gmail.com>
2026-09-13 16:11:15 +0530

    message

---
+++ frontend/script.js
@@ -0,0 +1,96 @@
+const API_URL = "http://localhost:3000/api/todos";
+
+async function getTodos() {
+
+    const response = await fetch(API_URL);
+
+    const todos = await response.json();
+
+    const list = document.getElementById("todoList");
+
+    list.innerHTML = "";
+
+    todos.forEach(todo => {
+
+        const li = document.createElement("li");
+
+        li.innerHTML = `
+            <span>
+                ${todo.title}
+                ${todo.completed ? " ✅" : ""}
+            </span>
+
+            <button onclick="updateTodo('${todo._id}', ${todo.completed})">
+                Complete
+            </button>
+
+            <button onclick="deleteTodo('${todo._id}')">
+                Delete
+            </button>
+        `;
+
+        list.appendChild(li);
+    });
+}
+
+
+async function addTodo() {
+
+    const input = document.getElementById("todoInput");
+
+    const title = input.value;
+
+    if (!title) {
+        return;
+    }
+
+    await fetch(API_URL, {
+
+        method: "POST",
+
+        headers: {
+            "Content-Type": "application/json"
+        },
+
+        body: JSON.stringify({
+            title: title
+        })
+    });
+
+    input.value = "";
+
+    getTodos();
+}
+
+
+async function updateTodo(id, completed) {
+
+    await fetch(`${API_URL}/${id}`, {
+
+        method: "PUT",
+
+        headers: {
+            "Content-Type": "application/json"
+        },
+
+        body: JSON.stringify({
+            completed: !completed
+        })
+    });
+
+    getTodos();
+}
+
+
+async function deleteTodo(id) {
+
+    await fetch(`${API_URL}/${id}`, {
+
+        method: "DELETE"
+    });
+
+    getTodos();
+}
+
+
+getTodos();
