a86a798c6ef9babefd887fbe080f5c9400abf626
JyothiYellam <jyothiym23@gmail.com>
2026-09-17 21:00:26 +0530

    message

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,3 @@
 const config = {
-    API_URL: "http://3.239.185.174:3000/api/todos"  # place backend PublicIp here
+    API_URL: "http://3.239.185.174:3000/api/todos"  // place your backend publicip here
 };
